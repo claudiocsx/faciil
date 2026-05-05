02a4868bfe21a5d03102d5c9dde27cecd374aa21
claudiocsx <claudiosasuke@gmail.com>
2026-05-05 12:17:35 -0300

    style: headers branco/85 com efeito glass - bg-white/85 backdrop-blur-xl

--- git-push-header-glass.bat
+++ git-push-header-glass.bat
@@ -2,7 +2,7 @@
 cd /d C:\Users\Ana Claudia\Documents\meupdv\meu-pdv
 
 "C:\Program Files\Git\bin\git.exe" add -A
-"C:\Program Files\Git\bin\git.exe" commit -m "style: headers branco puro com efeito glass - box-shadow leve"
+"C:\Program Files\Git\bin\git.exe" commit -m "style: headers branco/85 com efeito glass - bg-white/85 backdrop-blur-xl"
 "C:\Program Files\Git\bin\git.exe" push origin main
 
 echo.
